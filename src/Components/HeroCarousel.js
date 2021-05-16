88e0e18fea4d5722094b0bd1e2ea0174b40ee9a5
devil1321 <s.dominik1321@gmail.com>
2021-05-16 14:40:14 +0200

    carousel controls finished

--- src/Components/HeroCarousel.js
+++ src/Components/HeroCarousel.js
@@ -1,15 +1,36 @@
-import React,{useEffect,useRef,useContext} from 'react'
+import React,{useEffect,useRef,useContext,useState} from 'react'
 import { DataContext } from '../api/context'
+
 const HeroCarousel = () => {
-    // const { handleCarousel } = useContext(DataContext)
+    const [current,setCurrent] = useState(0)
+    const [move,setMove] = useState(0)
+    const [movedNext,setMovedNext] = useState(false)
+    const {handleCarousel,handleControls } = useContext(DataContext)
     const myCarousel = useRef()
     const myCarouselItem = useRef()
     const myCarouselWrapper = useRef()
+    const nextRef = useRef()
+    const prevRef = useRef()
     useEffect(()=>{
+        let prev = prevRef.current.classList.value
+        let next = nextRef.current.classList.value
         let carousel = myCarousel.current.classList.value
         let carouselItem = myCarouselItem.current.classList.value
+        if(current > 4){
+            setMove(0)
+            setCurrent(0)
+        }
+        if(current < -1){
+            setMove(0)
+            setCurrent(0)
+        }
+        if(movedNext){
+            handleControls(next,null,current,move,carousel,carouselItem,30)
+        }else{
+            handleControls(null,prev,current,move,carousel,carouselItem,30)
+        }
         // handleCarousel(carousel,carouselItem,30)
-    },[])
+    },[move,movedNext])
     return (
         <div className="hero__carousel-wrapper" ref={myCarouselWrapper}>
             <div className="hero__carousel" ref={myCarousel}>
@@ -38,6 +59,17 @@ const HeroCarousel = () => {
                     <img className="hero__carousel-slide" src="/assets/home/slide-3.png" alt="" />
                 </div>
             </div>
+            <div className="prev" onClick={()=>{
+              
+                setMovedNext(false)
+                setCurrent(current - 1)
+                setMove(move - (myCarouselItem.current.clientWidth + 30))}}
+                ref={prevRef}><i className="fa fa-arrow-left fa-2x"></i></div>
+            <div className="next" onClick={()=>{
+                setCurrent(current + 1)
+                setMovedNext(true)
+                setMove(move + (myCarouselItem.current.clientWidth + 30))}}
+                ref={nextRef}><i className="fa fa-arrow-right fa-2x"></i></div>
         </div>
     )
 }
--- src/api/context.js
+++ src/api/context.js
@@ -1,12 +1,12 @@
 import React,{ createContext, useState, useEffect } from 'react';
 import axios from 'axios'
+import Data from './copy.json'
 export const DataContext = createContext({
     model:{},
     detailModel:{},
     data:{},
     handleCarousel:()=>{},
-    handlePrev:()=>{},
-    handleNext:()=>{},
+    handleControls:()=>{},
     setModel:()=>{},
     setDetailModel:()=>{},
     setData:()=>{}
@@ -17,7 +17,6 @@ export const DataProvider = ({children}) => {
     const [data,setData] = useState({})
     const [model,setModel] = useState({})
     const [detailModel,setDetailModel] = useState({})
-
     const handleCarousel = (carousel,carouselItem,margin) => {
         let isSet = false
         if(!isSet){
@@ -31,9 +30,6 @@ export const DataProvider = ({children}) => {
         }
         setInterval(()=>{
             if(current < max - 3){
-                setTimeout(()=>{
-                    carousel.style.transition = 'all 1s ease-in-out'   
-                },1000)
                 current++
                 carousel.style.transform = `translateX(-${move}px)`
                 move += item.clientWidth + margin
@@ -42,36 +38,63 @@ export const DataProvider = ({children}) => {
                 carousel.style.transform = `translateX(${startMove}px)`
                 current = 0
                 move = item.clientWidth + margin
+                    setTimeout(()=>{
+                        carousel.style.transition = 'all 1s ease-in-out'   
+                    },1000)
                 }    
-            },2000)
+        },2000)
 
     }
-    const handlePrev = () =>{}
-    const handleNext = () =>{}
+
+    const handleControls = (next,prev,current,move,carousel,carouselItem,margin) =>{
+        var items = document.querySelectorAll(`.${carouselItem}`)
+        var carousel = document.querySelector(`.${carousel}`)
+        var width = items[0].clientWidth + margin
+        var max = items.length
+        var startMove = 0
+        if(current < max && prev === null){
+            carousel.style.transform = `translateX(-${move}px)`
+        }else if(current >= -1 && next === null){
+            if(move === -width){
+                move = move * (-1)
+                carousel.style.transform = `translateX(${move}px)`
+            }else{
+                move = move * (-1)
+                carousel.style.transform = `translateX(${move}px)`
+            }
+        }else if(current === max || current === -2){
+            carousel.style.transform = `translateX(${startMove}px)`
+        }
+    }
 
     useEffect(()=>{
-        const options = {
-            method: 'GET',
-            url: 'https://streaming-availability.p.rapidapi.com/search/basic',
-            params: {
-              country: 'us',
-              service: 'netflix',
-              type: 'movie',
-              genre: '18',
-              page: '1',
-              language: 'en'
-            },
-            headers: {
-              'x-rapidapi-key': 'ee01db358fmshf866f3732da81eap1aa530jsnb32207469154',
-              'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
-            }
-          };
+        // rapid api
+        // const options = {
+        //     method: 'GET',
+        //     url: 'https://streaming-availability.p.rapidapi.com/search/basic',
+        //     params: {
+        //       country: 'us',
+        //       service: 'netflix',
+        //       type: 'movie',
+        //       genre: '18',
+        //       page: '1',
+        //       language: 'en'
+        //     },
+        //     headers: {
+        //       'x-rapidapi-key': 'ee01db358fmshf866f3732da81eap1aa530jsnb32207469154',
+        //       'x-rapidapi-host': 'streaming-availability.p.rapidapi.com'
+        //     }
+        //   };
           
-          axios.request(options).then(function (response) {
-              console.log(response.data);
-          }).catch(function (error) {
-              console.error(error);
-          });
+        //   axios.request(options).then(function (response) {
+        //       console.log(response.data);
+        //   }).catch(function (error) {
+        //       console.error(error);
+        //   });
+        //   end rapid api
+        // api json
+        // end api json
+        setData(Data)
     },[])
 
     
@@ -80,8 +103,7 @@ export const DataProvider = ({children}) => {
             data,
             model,
             detailModel,
-            handlePrev,
-            handleNext,
+            handleControls,
             handleCarousel,
             setData,
             setModel,
